8b98bcb55743c5ebed9e585518171e265fafa2de
unknown <daveasmah@yahoo.com>
2022-07-27 20:57:37 -0400

    Final commit

--- app.js
+++ app.js
@@ -5,21 +5,22 @@ from an array (consider case sensitivity).*/
 let arrayOfFruits = ["Apple", "Mango", "apple", "orange", "Grapes",
                         "mango", "mango", "Avocado", "grapes"];
 
-// convert all array elements to lower case with a help of .map() to iterate over the array  
-let newFruits = arrayOfFruits.map(function (element) {
-    return element.toLowerCase();
-});
+
+
 
 // Write a function with an array as a parameter 
 function removeDuplicates(fruits){
 
+// convert all array elements to lower case with a help of .map() to iterate over the array      
+    let newFruits = fruits.map(function (element) {
+    return element.toLowerCase();
+    });        
+
 // return the filtered array using a the .filter() method 
-return fruits.filter(function (item, index) {
+    return newFruits.filter(function (item, index) {
 // remove first indexed elements only using the indexOf() method
-// Also use the .toLowerCase() method convert all strings to lower case    
-    return fruits.indexOf(item.toLowerCase()) === index;
-});    
+        return newFruits.indexOf(item) === index;
+    });    
 }
 
-console.log(removeDuplicates(newFruits)); 
-//
+console.log(removeDuplicates(arrayOfFruits)); 
